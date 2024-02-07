39165d19723f8446b26b2220e46fa1c816bcff99
Manish Bhurtel <bhmanish8848@gmail.com>
2024-02-07 12:18:59 +0545

    final changes done

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+//Selecting all the elements
 const searchBar = document.querySelectorAll('.search-bar')[0];
 const searchButton = document.querySelectorAll('.search-btn')[0];
 const cityDisplay = document.querySelectorAll('.city-name')[0];
@@ -22,14 +23,18 @@ const rainy_weather_img_url = './images/weather-images/rainy-weather.png'
 const thunderstorm_weather_img_url = './images/weather-images/thunderstorm.png'
 const broken_clouds_weather_img_url = './images/weather-images/broken-clouds.png'
 const ice_img_url = './images/weather-images/ice.png';
+const mist_img_url = './images/weather-images/mist.png';
+const snowfall_img_url = './images/weather-images/snowfall.png';
 
 //Initializing the current city to be displayed on screen
-let currentCity = `Pokhara`;
+let currentCity = `New York`;
 
+//Current state of loading is false
 let loading= false;
 
 //Open Weather API Key
-const api_key = `635fe4af6579b7d25efcd816868f2dca`;
+const api_key = `YOUR_API_KEY_HERE`;
+
 
 //Funtion to convert the provided city name into latitude and longitude first and then providing that details to another function to finally fetch the weather data
 const convertCityToCoordinates = async (city) => {
@@ -37,6 +42,7 @@ const convertCityToCoordinates = async (city) => {
         const response = await fetch(url);
         const data  =  await response.json();
 
+        //If there is weather data then fetch the weather data else simply alert a message
         if(!data.length == 0){
             let latitude = data[0].lat;
             let longitude = data[0].lon;
@@ -52,6 +58,7 @@ const convertCityToCoordinates = async (city) => {
             }
             showLoading();
 
+            //This function actually fetches the data based on latitude and longitude passed 
             const fetchData = async (lat , lon) => {
                 const final_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=standard`;
                 const final_response = await fetch(final_url);
@@ -62,7 +69,7 @@ const convertCityToCoordinates = async (city) => {
                 descriptionDisplay.innerHTML = api_desc;
         
                 //Conditionally rendering the images locally on the basis of description provided
-                const imageRendering = (sunny , thunderstorm , rainy , broken_clouds , ice) => {
+                const imageRendering = (sunny , thunderstorm , rainy , broken_clouds , ice , mist , snowfall) => {
                     if (api_desc.includes('clouds') || api_desc.includes('cloud') || api_desc.includes('fog')) {
                         imageDisplay.innerHTML = `<img src="${broken_clouds}" alt="Weather Icon" class="weather-current-image">`;
                     }
@@ -76,31 +83,36 @@ const convertCityToCoordinates = async (city) => {
                         imageDisplay.innerHTML = `<img src="${rainy}" alt="Weather Icon" class="weather-current-image">`;
                     }      
                     else if(api_desc.includes('snow') || api_desc.includes('ice')){
-                        imageDisplay.innerHTML = `<img src="${ice}" alt="Weather Icon" class="weather-current-image">`;
-                    }             
+                        imageDisplay.innerHTML = `<img src="${snowfall}" alt="Weather Icon" class="weather-current-image">`;
+                    }    
+                    else if(api_desc.includes('mist') || api_desc.includes('fog')){
+                        imageDisplay.innerHTML = `<img src="${mist}" alt="Weather Icon" class="weather-current-image">`;
+                    }            
                 }
-                imageRendering(sunny_weather_img_url  , thunderstorm_weather_img_url , rainy_weather_img_url , broken_clouds_weather_img_url , ice_img_url);
+                imageRendering(sunny_weather_img_url  , thunderstorm_weather_img_url , rainy_weather_img_url , broken_clouds_weather_img_url , ice_img_url , mist_img_url , snowfall_img_url);
                 
         
                 //Converting temperature in kelvin to degree celcius
                 let temperature_in_kelvin = final_data.main.temp;
                 let temperature_in_celcius =  temperature_in_kelvin - 273.15
                 temperatureDisplay.innerHTML = `${Math.floor(temperature_in_celcius)}°`;
+
+                //Displaying some weather information
                 windSpeedDisplay.innerHTML = `${final_data.wind.speed}m/s`;
                 windDegreeDisplay.innerHTML = `${final_data.wind.deg}deg`;
                 humidityDisplay.innerHTML = `${final_data.main.humidity}%`;
                 pressureDisplay.innerHTML = `${final_data.main.pressure}hPa`;
-                //Condition
+                //Conditionally displaying the rain information based on rain data
                 if(final_data.rain){
                     rainDisplay.innerHTML = `${final_data.rain['1h']}mm`;
                 }else{
                     rainDisplay.innerHTML = `0mm`;
                 }
+
+                //Real Feel temperature displaying
                 let realFeelTemp = (final_data.main.feels_like - 273.15);
                 realFeelDisplay.innerHTML = `${Math.floor(realFeelTemp)}°`;
-        
-                // console.log(final_data)
-        
+
                 //Show loading=false when data fetching is completed
                 const hideLoading = () => {
                     loading = false;
@@ -120,18 +132,18 @@ const convertCityToCoordinates = async (city) => {
             alert('No cities found!!!');
         }
 }
-       
+//This function is called to initally display the weather data before user searches for any cities
 convertCityToCoordinates(currentCity);
 
 
-
+//This function calls the fetch function to fetch the weather data based on the city provided by user
 const conditionalFetchData = () => {
     currentCity = searchBar.value;
     convertCityToCoordinates(currentCity);
 }
 
-// const regEx = /[^a-zA-Z\s]/g;
-
+//This function validates the search input(If user puts empty data or space in the search bar, it will pop an alert message)
+//When clicked on the enter keyword in keyboard
 searchBar.addEventListener('keyup' ,  (e) =>{
     if(e.key == 'Enter') {
         if(!searchBar.value.trim() == ''){
@@ -141,7 +153,8 @@ searchBar.addEventListener('keyup' ,  (e) =>{
         }
     }
 })
-
+//This function validates the search input(If user puts empty data or space in the search bar, it will pop an alert message)
+//When clicked on the search button
 searchButton.addEventListener('click' ,  () =>{
     if(!searchBar.value.trim() == ''){
         conditionalFetchData();
